8e333ca85c06c6a5adaf16f8d982fb19abddacd1
TarunYadav3011 <tytarunyadav0008@gmail.com>
2026-08-11 20:58:57 +0530

    Step-9 ---add auth endpoint

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,51 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
+// bcrypt , zod, jsonwebtoken
 
+const JWT_ADMIN_PASSWORD = "11111323";
 
-adminRouter.post("/signup", function(req, res){
+adminRouter.post("/signup", async function(req, res){
+    const { email, password, firstName, lastName } = req.body; //todo adding zod validation
+    //tod: hash the password so plain password doesn't directly store in db
+
+    
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+        })
+
     res.json({
-        message: 'signup endpoint'
+        message: 'signup succeeded'
     })
 })
 
-adminRouter.post("/signin", function(req, res){
-    res.json({
-        message: 'signin endpoint'
+
+adminRouter.post("/signin", async function(req, res){
+    const { email , password } = req.body;
+
+    //ideally password should be hashed, and hence you can't comapre the suer provided password and the database password
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
     })
+    if (admin) {
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_ADMIN_PASSWORD);
+
+        //do cookie logic
+
+        res.json({
+            token: token
+        })
+        } else {
+            res.status(403).json({
+                message: "Iccorrect credentials"
+            })
+        }
 })
 
 adminRouter.post("/course", function(req, res){
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,51 @@
-
 const { Router } = require("express");
+const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "aeerthrteddr";
 
 const userRouter = Router();
 
-userRouter.post("/signup", function(req, res){
+userRouter.post("/signup", async function(req, res){
+    const { email, password, firstName, lastName } = req.body; //todo adding zod validation
+    //tod: hash the password so plain password doesn't directly store in db
+
+    
+    await userModel.create({
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+        })
+
     res.json({
-        message: 'signup endpoint'
+        message: 'signup succeeded'
     })
 })
 
-userRouter.post("/signin", function(req, res){
-    res.json({
-        message: 'signin endpoint'
+
+userRouter.post("/signin", async function(req, res){
+    const { email , password } = req.body;
+
+    //ideally password should be hashed, and hence you can't comapre the suer provided password and the database password
+    const user = await userModel.findOne({
+        email: email,
+        password: password
     })
+    if (user){
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_USER_PASSWORD);
+
+        //do cookie logic
+
+        res.json({
+            token: token
+        })
+        } else {
+            res.status(403).json({
+                message: "Iccorrect credentials"
+            })
+        }
 })
 
 userRouter.get("/purchases", function(req, res){
