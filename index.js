b191358c52ad709d6519848141176b6e5d26031b
TarunYadav3011 <tytarunyadav0008@gmail.com>
2026-08-11 12:36:32 +0530

    Step-5 Added Admin rote

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { createUserRoute } = require("./routes/user");
 const { createCourseRoute } = require("./routes/user");
-
+const { adminRouter } = require("./routes/admin");
 const app = express();
 
 
-app.use("/user",userRouter);
-app.use("/course",courseRouter);
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/admin",adminRouter);
+app.use("/api/v1/course",courseRouter);
 
 
 app.listen(3000); 
